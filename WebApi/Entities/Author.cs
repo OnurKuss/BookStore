b64c8b4eb07607f0347a053fca088cad6bbe8072
onur kuş <onurkus74@gmail.com>
2022-04-04 18:22:20 +0300

    Entities Added

---
+++ WebApi/Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Author
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDay { get; set; }
+        public bool IsBookPublished { get; set; } = false;
+    }
+}
---
+++ WebApi/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Entities
+{
+    public class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Tittle { get; set; }
+        public int PageCount { get; set; }
+        public DateTime PublishDate { get; set; }
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        public int AuthorId { get; set; }
+        public Author Author { get; set; }
+    }
+}
