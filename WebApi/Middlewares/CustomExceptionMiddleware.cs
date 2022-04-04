17b8c3101cebe8e17f52418fdecf9553372316e1
onur kuş <onurkus74@gmail.com>
2022-04-04 18:23:25 +0300

    Middleware added!

---
+++ WebApi/Middlewares/CustomExceptionMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebApi.Services;
+
+namespace WebApi.Middlewares
+{
+    public class CustomExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        private readonly ILoggerService _loggerService;
+
+        public CustomExceptionMiddleware(RequestDelegate next, ILoggerService loggerService)
+        {
+            _next = next;
+            _loggerService = loggerService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var watch = Stopwatch.StartNew();
+
+            try
+            {
+                string message = "[Request] HTTP " + context.Request.Method + " - " + context.Request.Path;
+                // Console.WriteLine(message);
+                // await context.Response.WriteAsync(message);
+                _loggerService.Write(message);
+                await _next(context);
+
+                message = "[Response] HTTP " + context.Request.Method + " - " + context.Request.Path +
+                " responded " + context.Response.StatusCode + " in " + watch.Elapsed.TotalMilliseconds + " ms ";
+                _loggerService.Write(message);
+                await context.Response.WriteAsync(message);
+            }
+            catch (Exception ex)
+            {
+                watch.Stop();
+                await HandleException(context, ex, watch);
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception ex, Stopwatch watch)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            string message = "[Error] HTTP " + context.Request.Method + " - " +
+                context.Response.StatusCode + " Error Message " + ex.Message + " in " + watch.Elapsed.TotalMilliseconds + "ms";
+            _loggerService.Write(message);
+
+            var result = JsonConvert.SerializeObject(new { error = ex.Message }, Formatting.None);
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+
+    public static class CustomExceptionMiddlewareExtension
+    {
+        public static IApplicationBuilder UseCustomExceptionMiddle(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CustomExceptionMiddleware>();
+        }
+    }
+}
