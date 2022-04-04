bca99c6e378362b4cbf00ad72dadca6f14f818a1
onur kuş <onurkus74@gmail.com>
2022-04-04 18:13:44 +0300

    Genre

---
+++ WebApi/Applications/GenreOperations/Queries/GetGenres/GetGenresQuery.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.DBOperations;
+
+namespace WebApi.Applications.GenreOperations.Queries.GetGenres
+{
+    public class GetGenresQuery
+    {
+        public readonly BookStoreDbContext _context;
+        public readonly IMapper _mapper;
+
+        public GetGenresQuery(BookStoreDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public List<GenresViewModel> Handle()
+        {
+            var genres = _context.Genres.Where(x => x.IsActive).OrderBy(x => x.Id);
+            List<GenresViewModel> resultObj = _mapper.Map<List<GenresViewModel>>(genres);
+            return resultObj;
+        }
+
+    }
+
+    public class GenresViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
