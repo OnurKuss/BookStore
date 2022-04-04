e9ca504b3a8ab651bb41744685c9be37e4fd72ae
onur kuş <onurkus74@gmail.com>
2022-04-04 18:24:04 +0300

    Data refactoring

--- WebApi/DBOperations/BookStoreDbContext.cs
+++ WebApi/DBOperations/BookStoreDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Entities;
 
 namespace WebApi.DBOperations
 {
@@ -11,5 +12,7 @@ namespace WebApi.DBOperations
         public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options):base(options)
         {  }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Author> Authors { get; set; }
     }
 }
--- WebApi/DBOperations/DataGenerator.cs
+++ WebApi/DBOperations/DataGenerator.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApi.Entities;
 
 namespace WebApi.DBOperations
 {
@@ -17,28 +18,69 @@ namespace WebApi.DBOperations
                 {
                     return;
                 }
+
+                context.AddRange(
+                    new Author
+                    {
+                        FirstName = "Eric",
+                        LastName = "Ries",
+                        BirthDay = "05.06.1895",
+                        IsBookPublished=true
+                    },
+                    new Author
+                    {
+                        FirstName = "Charlotte",
+                        LastName = "Perkins Gilman",
+                        BirthDay = "01.01.1976",
+                        IsBookPublished = true
+                    },
+                    new Author
+                    {
+                        FirstName = "Frank",
+                        LastName = "Herbert",
+                        BirthDay = "01.01.1985",
+                        IsBookPublished = true
+                    });
+
+                context.AddRange(
+                    new Genre
+                    {
+                        Name = "Personel Growth",
+                    },
+                    new Genre
+                    {
+                        Name = "Science Fiction",
+                    },
+                    new Genre
+                    {
+                        Name = "Noval",
+                    });
+
                 context.Books.AddRange(
                 new Book
                 {
-                   // Id = 1,
+                    // Id = 1,
                     Tittle = "Lean Startup",
                     GenreId = 1, //Personal Growth
+                    AuthorId=1, //Eric Ries
                     PageCount = 200,
                     PublishDate = new DateTime(2021, 05, 11)
                 },
                 new Book
                 {
-                  //  Id = 2,
+                    //  Id = 2,
                     Tittle = "Herland",
                     GenreId = 2, //Science Fiction
-                    PageCount = 250,
+                    AuthorId = 2,
+                    PageCount = 250, //
                     PublishDate = new DateTime(2010, 08, 18)
                 },
                 new Book
                 {
-                   // Id = 3,
+                    // Id = 3,
                     Tittle = "Dune",
-                    GenreId = 2,
+                    GenreId = 3,
+                    AuthorId = 3,
                     PageCount = 540,
                     PublishDate = new DateTime(2021, 12, 21)
                 });
