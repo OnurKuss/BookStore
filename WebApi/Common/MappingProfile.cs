1a5b59a69ae530be54f211a4f1bb6bf4de237c42
onur kuş <onurkus74@gmail.com>
2022-04-04 18:13:01 +0300

    Mapping Added

--- WebApi/Common/MappingProfile.cs
+++ WebApi/Common/MappingProfile.cs
@@ -3,7 +3,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using WebApi.BookOperations;
+using WebApi.Applications.AuthorOperations.Commands.CreateAuthor;
+using WebApi.Applications.BookOperations;
+using WebApi.Applications.GenreOperations.Queries.GetGenres;
+using WebApi.Entities;
+using static WebApi.Applications.AuthorOperations.Queries.GetAuthorDetail.GetAuthorDetailQuery;
+using static WebApi.Applications.AuthorOperations.Queries.GetAuthors.GetAuthorsQuery;
+using static WebApi.Applications.GenreOperations.Queries.GetGenreDetail.GetGenreDetailQuery;
 
 namespace WebApi.Common
 {
@@ -12,8 +18,15 @@ namespace WebApi.Common
         public MappingProfile()
         {
             CreateMap<CreateBookModel, Book>();
-            CreateMap<Book, BookIdViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
-            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => ((GenreEnum)src.GenreId).ToString()));
+            CreateMap<Book, BookIdViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name));
+            CreateMap<Book, BooksViewModel>().ForMember(dest => dest.Genre, opt => opt.MapFrom(src => src.Genre.Name))
+                                      .ForMember(dest=> dest.Author, opt=> opt.MapFrom(src=> src.Author.FirstName+ " " +src.Author.LastName));
+            CreateMap<Genre, GenresViewModel>();
+            CreateMap<Genre, GenreDetailViewModel>();
+            CreateMap<CreateAuthorModel, Author>();
+            CreateMap<Author, AuthorDetailViewModel>();
+            CreateMap<Author, AuthorViewModel>();
+
         }
     }
 }
